eb1230f752b7dcfbb771193148f473b7fbe31f50
Jadan Morrow <jadanmorrow@outlookcom>
2020-01-14 20:39:15 -0700

    Initial Commit

---
+++ LogIn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogIn
+//
+//  Created by Jadan Morrow on 1/14/20.
+//  Copyright © 2020 Jadan Morrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
